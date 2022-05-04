44018c04d57a4ecc300dd4760a55af94f03b31af
Zayden <zayden@Zayden-EX-MAC.local>
2022-05-04 15:40:10 +0900

    feat: msw를 사용하여 스토리북에서 api 요청 mocking

--- src/components/InboxScreen/InboxScreen.stories.tsx
+++ src/components/InboxScreen/InboxScreen.stories.tsx
@@ -1,11 +1,52 @@
 import InboxScreen from "./InboxScreen";
+import { rest } from "msw";
+import { ComponentStory } from "@storybook/react";
+import * as TaskStories from "../Task/Task.stories";
 
 export default {
   component: InboxScreen,
   title: "InboxScreen",
 };
 
-const Template = () => <InboxScreen />;
+const Template: ComponentStory<typeof InboxScreen> = () => <InboxScreen />;
 
-export const Default = Template.bind({});
-export const Error = Template.bind({});
+const MockedState = {
+  tasks: [
+    { ...TaskStories.Default.args?.task, id: "1", title: "Task 1" },
+    { ...TaskStories.Default.args?.task, id: "2", title: "Task 2" },
+    { ...TaskStories.Default.args?.task, id: "3", title: "Task 3" },
+    { ...TaskStories.Default.args?.task, id: "4", title: "Task 4" },
+    { ...TaskStories.Default.args?.task, id: "5", title: "Task 5" },
+    { ...TaskStories.Default.args?.task, id: "6", title: "Task 6" },
+  ],
+  status: "idle",
+  error: null,
+};
+
+export const Default: ComponentStory<typeof InboxScreen> = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      ),
+    ],
+  },
+};
+
+export const Error: ComponentStory<typeof InboxScreen> = Template.bind({});
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
